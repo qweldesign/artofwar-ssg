6fbef452876918e473b9b1782472878a7cd01afb
QWEL.DESIGN <webmaster@qwel.design>
2025-09-03 13:43:56 +0900

    Adds a minimal UI

---
+++ init.js
@@ -0,0 +1,13 @@
+// Auto Copyright
+import AutoCopyright from './js/autoCopyright.js';
+new AutoCopyright(2024, 'ArtOfWar.Site');
+
+// Back To Top
+import BackToTop from './js/backToTop.js';
+new BackToTop();
+
+// Drawer Menu
+import DrawerMenu from './js/drawerMenu.js';
+new DrawerMenu({
+  primaryMenu: document.querySelector('.gNav__menu')
+});
